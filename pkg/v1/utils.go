12d6ddbb4860e6574be4e15f44ae146a1d833112
4thel00z <4thel00z@gmail.com>
2020-12-27 03:34:59 +0100

    [master] Update the utils package
    Signed-off-by: 4thel00z <4thel00z@gmail.com>

--- pkg/v1/utils.go
+++ pkg/v1/utils.go
@@ -1,6 +1,12 @@
 package v1
 
-import "strings"
+import (
+	"crypto/rand"
+	"encoding/json"
+	"errors"
+	"math/big"
+	"strings"
+)
 
 func ContainsString(needle string, haystack ...string) bool {
 	for _, hay := range haystack {
@@ -37,4 +43,45 @@ func All(truths ...bool) bool {
 		}
 	}
 	return true
+}
+
+func Send(obj interface{}, output chan<- []byte) error {
+	payload, err := json.Marshal(obj)
+	if err != nil {
+		return err
+	}
+	output <- payload
+	return nil
+}
+
+func Max(array []float64) (float64, error) {
+	if len(array) == 0 {
+		return -1, errors.New("len(array) == 0")
+	}
+	max := array[0]
+	for _, value := range array {
+		if max < value {
+			max = value
+		}
+	}
+	return max, nil
+}
+
+func Min(array []float64) (float64, error) {
+	if len(array) == 0 {
+		return -1, errors.New("len(array) == 0")
+	}
+	min := array[0]
+	for _, value := range array {
+		if min > value {
+			min = value
+		}
+	}
+	return min, nil
+}
+
+func InitialSequenceNumber(max int64) int64 {
+	n := big.NewInt(max)
+	b, _ := rand.Int(rand.Reader, n)
+	return b.Int64()
 }
